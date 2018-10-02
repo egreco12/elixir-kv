663dc43813d055290310c7b32029efce91ed6233
Evan Greco <evan.greco@legitscript.com>
2018-10-01 20:00:24 -0700

    Add delete

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -1,11 +1,18 @@
 defmodule KV.BucketTest do
     use ExUnit.Case, async: true
 
-    test "stores value by key" do
+    setup do
         {:ok, bucket} = KV.Bucket.start_link([])
+        %{bucket: bucket}
+    end
+
+    test "stores value by key", %{bucket: bucket} do
         assert KV.Bucket.get(bucket, "milk") == nil
 
         KV.Bucket.put(bucket, "milk", 3)
         assert KV.Bucket.get(bucket, "milk") == 3
+
+        assert KV.Bucket.delete(bucket, "milk") == 3
+        assert KV.Bucket.get(bucket, "milk") == nil
     end
 end
